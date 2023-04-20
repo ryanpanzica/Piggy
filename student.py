9c59678b8f9516655ff771ea92459b42dbfcea8f
ryanpanzica <111909703+ryanpanzica@users.noreply.github.com>
2023-04-20 19:13:15 +0000

    its been pleasure

--- student.py
+++ student.py
@@ -41,7 +41,7 @@ class Piggy(PiggyParent):
                 "f": ("Follow", self.follow),
                 "c": ("Calibrate", self.calibrate),
                 "q": ("Quit", self.quit),
-                "P": ("Panzica", self.panzica)
+                "p": ("panzica", self.panzica)
                 }
         # loop and print the menu...
         for key in sorted(menu.keys()):
