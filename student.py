f5773767f34285a60f8a3a07131921e69d52ba22
ryanpanzica <111909703+ryanpanzica@users.noreply.github.com>
2023-05-01 19:00:39 +0000

    move wall

--- student.py
+++ student.py
@@ -96,7 +96,15 @@ class Piggy(PiggyParent):
             return False
           else:
             return True
-      
+    def wall(self):
+        
+        while True:
+          self.fwd()
+          for ang in range(self.MIDPOINT-400, self.MIDPOINT+401, 100):
+            self.servo(ang)
+            time.sleep(.1)
+            if self.read_distance() < 250:
+              self.stop()
 
     def shake(self):
         """ Another example move """
@@ -155,12 +163,4 @@ if __name__ == "__main__":  # only run this loop if this is the main file
     except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
         p.quit()  
 
-    def wall(self):
-        
-        while True:
-          self.fwd()
-          for ang in range(self.MIDPOINT-400, self.MIDPOINT+401, 100):
-            self.servo(ang)
-            time.sleep(.1)
-            if self.read_distance() < 250:
-              self.stop()
+    
