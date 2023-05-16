12d4fdd9faf4e6b961fe1046e18004291fab607a
ryanpanzica <111909703+ryanpanzica@users.noreply.github.com>
2023-05-16 19:01:52 +0000

    adf

--- student.py
+++ student.py
@@ -18,7 +18,7 @@ class Piggy(PiggyParent):
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
         self.LEFT_DEFAULT = 80
-        self.RIGHT_DEFAULT = 80
+        self.RIGHT_DEFAULT = 79
         self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
@@ -188,7 +188,7 @@ class Piggy(PiggyParent):
             time.sleep(2)
         
     def maze_1(self):
-      while True: 
+      while True:
         self.fwd()
         self.servo(self.MIDPOINT)
         time.sleep(.1)
