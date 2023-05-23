e00a713b692bab418277908501e6cc1c23058ee6
ryanpanzica <111909703+ryanpanzica@users.noreply.github.com>
2023-05-23 18:09:55 +0000

    70

--- student.py
+++ student.py
@@ -205,7 +205,7 @@ class Piggy(PiggyParent):
           lt_distance = self.read_distance()
           
           if lt_distance > rt_distance:
-            self.turn_by_deg(-72)
+            self.turn_by_deg(-70)
             self.stop()
           elif rt_distance > lt_distance:
             self.turn_by_deg(75)
